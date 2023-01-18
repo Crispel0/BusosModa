323ee9d9b6fc0f96475f407546b186424e99eadd
Crispelo <penagoscristian61@gmail.com>
2023-01-18 18:10:54 -0500

    beggin project BusosModa

---
+++ busosmoda/src/main/java/com/busosmoda/mvc/busosmoda/BusosmodaApplication.java
@@ -0,0 +1,13 @@
+package com.busosmoda.mvc.busosmoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusosmodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusosmodaApplication.class, args);
+	}
+
+}
---
+++ busosmoda/src/test/java/com/busosmoda/mvc/busosmoda/BusosmodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busosmoda.mvc.busosmoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusosmodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
