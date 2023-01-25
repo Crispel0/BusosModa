c17c6d34398fe1818aaf01f39e1b463d99a38204
Crispelo <penagoscristian61@gmail.com>
2023-01-25 12:17:55 -0500

    adding expression language for have differents routes and answer of status depend select a nav

--- busosmoda/src/main/java/com/busosmoda/mvc/busosmoda/controller/homecontroller.java
+++ busosmoda/src/main/java/com/busosmoda/mvc/busosmoda/controller/homecontroller.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
+import org.springframework.util.ErrorHandler;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.busosmoda.mvc.busosmoda.model.Pedido;
@@ -31,15 +34,30 @@ public class homecontroller {
 		return "home";
 	}
 	
-	@GetMapping("/esperando")
-	public String esperando(Model model) {
+	
+	/*Metodo que recibe un status de una peticion dependiendo si es ESPERANDO, APROBADO, TERMINADO*/
+	@GetMapping("/{status}")
+	//el path variable toma la expression language que esta como ruta hacia la String status 
+	public String status(@PathVariable String status, Model model) {
+		/*encuentra por status y convierte de tipo String a enum y por ultimo el String de status lo convierte a mayuscula 
+		porque los valores del enum estan en mayuscula*/
 		
-		List<Pedido> pedidos = repository.findBystatus(StatusPedido.ESPERANDO);
+		List<Pedido> pedidos = repository.findBystatus(StatusPedido.valueOf(status.toUpperCase()));
 		
 		
 		model.addAttribute("pedidos", pedidos);
+		model.addAttribute("status", status);
 		
 		return "home";
 	}
+	
+	/*En el momento de escribir cualquier otra direccion URL de las especificadas automaticamente redirige a la home porque
+	salta la exception IllegalArgumentExcepion*/
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String Error() {
+		
+		return "redirect:/home";
+	}
 }
+
 
