e23b927f2f0a41822ca29471ea326295870d36ff
Crispelo <penagoscristian61@gmail.com>
2023-01-25 11:15:22 -0500

    add a filter for the status  of each article

--- busosmoda/src/main/java/com/busosmoda/mvc/busosmoda/controller/homecontroller.java
+++ busosmoda/src/main/java/com/busosmoda/mvc/busosmoda/controller/homecontroller.java
@@ -7,11 +7,14 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.busosmoda.mvc.busosmoda.model.Pedido;
+import com.busosmoda.mvc.busosmoda.model.StatusPedido;
 import com.busosmoda.mvc.busosmoda.repository.PedidoRepository;
 
 @Controller
+@RequestMapping("/home")
 public class homecontroller {
 	
 	@Autowired
@@ -20,10 +23,18 @@ public class homecontroller {
 	/*toma todos los pedidos y los guarda en una lista
 	de pedidos despues los agrega a el modelo y lo llama pedidos para usarlo en las views*/
 	
-	@GetMapping("/home")
+	@GetMapping
 	public String home(Model model) {
 		
 		List<Pedido> pedidos = repository.findAll();
+		model.addAttribute("pedidos",pedidos);
+		return "home";
+	}
+	
+	@GetMapping("/esperando")
+	public String esperando(Model model) {
+		
+		List<Pedido> pedidos = repository.findBystatus(StatusPedido.ESPERANDO);
 		
 		
 		model.addAttribute("pedidos", pedidos);
--- busosmoda/src/main/java/com/busosmoda/mvc/busosmoda/repository/PedidoRepository.java
+++ busosmoda/src/main/java/com/busosmoda/mvc/busosmoda/repository/PedidoRepository.java
@@ -1,9 +1,12 @@
 package com.busosmoda.mvc.busosmoda.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.busosmoda.mvc.busosmoda.model.Pedido;
+import com.busosmoda.mvc.busosmoda.model.StatusPedido;
 /**
  * La clase PedidosRepository esta encargada de toda la persistencia de los datos para la base de datos
  * @author Usuario
@@ -13,5 +16,8 @@ import com.busosmoda.mvc.busosmoda.model.Pedido;
 	@Repository /*not is necessary implement findAll because in the into them this implement this option 
 	no es necesario implementar findAll porque in el interiro de ella esta opcion es autoconfigurada*/
 	public interface PedidoRepository extends JpaRepository<Pedido,Long> {
+
+	/*hace un filtrado por status */
+	List<Pedido> findBystatus(StatusPedido status);
 		
 }
