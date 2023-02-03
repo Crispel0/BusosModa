91b105c2f3ec521fc09933783bba1f5644760ea3
Crispelo <penagoscristian61@gmail.com>
2023-02-03 17:06:17 -0500

    configuration of httpSecurity and WebSecurity

--- busosmoda/src/main/java/com/busosmoda/mvc/config/SecurityConfig/SecurityConfig.java
+++ busosmoda/src/main/java/com/busosmoda/mvc/config/SecurityConfig/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.busosmoda.mvc.config.SecurityConfig;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,5 +12,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Override /** configure the authoentication of users - configuracion de autoentificacion de usuarios*/
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		super.configure(auth);
+	}
+	
+	@Override /*protect the URLS access - onfiguration de control de acceso*/
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/home").permitAll().
+		antMatchers(HttpMethod.GET,"/home/*").permitAll();
+			
+	}
+	
+	@Override /* Conifguration for all the fields estaticos - configuracion para los archivos estaticos (js ccs img)*/
+	public void configure(WebSecurity web) throws Exception {
+		super.configure(web);
+	}
+	
 }
 
